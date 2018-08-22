adf6e1bf624c5251e15f90feb039aa3d13649e32
Olivier Willems <ojwillems@gmail.com>
2018-08-22 02:24:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split
   array.collect do |reverse_sentence|
     reverse_sentence.reverse do |x|
-      puts x
+      x
     end
   end.join(" ")
   end
